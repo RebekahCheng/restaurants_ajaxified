6afcb77a9ec097ba774417365b91e6b58ffb27a3
Rebekah Cheng <work@rebekahcheng.com>
2017-06-03 14:43:10 +0100

    Added restaurant index and show views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,11 @@
+class RestaurantsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
