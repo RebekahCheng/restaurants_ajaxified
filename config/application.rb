a171e144236bed484c441bddd9cb0d65684716d8
Rebekah Cheng <work@rebekahcheng.com>
2017-06-05 11:03:46 +0100

    edited search bar

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,10 @@ module RestaurantsAjaxified
   class Application < Rails::Application
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
-    config.generators do |generate|
-      generate.assets false
-      generate.helper false
-    end
+    # config.generators do |generate|
+    #   generate.assets false
+    #   generate.helper false
+    # end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
